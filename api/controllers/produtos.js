31640ef5deb5d84c2bc85449bd8988088bacefe7
clesiorods <clesiorodrigues96@gmail.com>
2022-08-31 22:51:42 -0300

    Adição do Sequelize, e configuração de conexão com banco de teste

--- api/controllers/produtos.js
+++ api/controllers/produtos.js
@@ -1,5 +1,15 @@
+const { Sequelize } = require('sequelize');
+const Produtos = require("../models/produtos");
+
 module.exports = {
-    getAll(request, response) {
-        response.status(200).send({message: "Tudo deu certo"})
+    async getAll(request, response) {
+        const sequelize = new Sequelize('banco_teste', 'root', '', {
+            host: 'localhost',
+            dialect: 'mysql'
+        });
+
+        const produtos = await Produtos(sequelize, Sequelize.DataTypes).findAll();
+
+        response.status(200).send({ produtos: produtos})
     }
 }
